41f46304c84735843c7710b88424f66591bbcd84
Aleksandar Ivanov <alex9408@icloud.com>
2018-05-26 21:43:10 +0300

    working

---
+++ handlers/index.js
@@ -0,0 +1,6 @@
+const homeHandler = require("./home");
+const filesHandler = require('./static-files');
+const productHandler = require('./product');
+
+
+module.exports= [homeHandler, filesHandler, productHandler];
